1b7674b8e586626fe83276a1b52d70b31e7bc9f1
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2024-12-21 15:05:47 -0300

    Adição de interface para apresentar informações de candidatos a projetos

--- src/app/componentes/InfoCandidato/InfoCandidato.js
+++ src/app/componentes/InfoCandidato/InfoCandidato.js
@@ -1,5 +1,7 @@
 import './InfoCandidato.css'
 import { getCandidatosById } from '@/lib/candidato'
+import FotoTeste from './../../../../public/logo-google.png'
+import Image from 'next/image';
 
 export default async function InfoCandidato({ id }){
 
@@ -8,8 +10,24 @@ export default async function InfoCandidato({ id }){
     const dados = candidato;
 
     return(
-        <div>
-            <p>O id {dados.id} é do candidato {dados.nome}</p>
+        <div className="info-candidatos">
+            <section className='sect-info1'>
+                <Image className="foto-candidato" src={FotoTeste} alt="Foto de perfil do candidato"></Image>
+            </section>
+            <section className='sect-info2'>
+                <h2 className="titulos">Nome:</h2>
+                    <div className="sessoes">{dados.nome}</div>
+                <h2 className="titulos">Contato:</h2>
+                    <div className="sessoes">{dados.contato}</div>
+                <h2 className="titulos">CRE:</h2>
+                    <div className="sessoes">{dados.cre}</div>
+            </section>
+            <section className='sect-info3'>
+                <h2 className="titulos">Email:</h2>
+                    <div className="sessoes">{dados.email}</div>
+                <h2 className="titulos">Matrícula:</h2>
+                    <div className="sessoes">{dados.matricula}</div>
+            </section>
         </div>
     )
 }
--- src/app/rotas/candidato/[id]/page.js
+++ src/app/rotas/candidato/[id]/page.js
@@ -1,10 +1,13 @@
 import styles from "./page.module.css";
+import NavBarGeral from "@/app/componentes/NavBarGeral/NavBarGeral";
 import InfoCandidato from "@/app/componentes/InfoCandidato/InfoCandidato";
 
 export default async function Candidato({ params }){
 
     return(
         <div>
+            <NavBarGeral></NavBarGeral>
+            <h1 className={styles.titulo}>Perfil do candidato: {/*projeto.nome*/}</h1>
             <InfoCandidato id={params.id}/>
         </div>
     )
