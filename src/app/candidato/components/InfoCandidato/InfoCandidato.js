7b99b9d0cdc510e7b124ae13612dc8cf0f9f3665
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-01-30 21:34:44 -0300

    Organização de elementos na tela de informações do candidato

--- src/app/candidato/components/InfoCandidato/InfoCandidato.js
+++ src/app/candidato/components/InfoCandidato/InfoCandidato.js
@@ -11,22 +11,27 @@ export default async function InfoCandidato({ id }){
 
     return(
         <div className="info-candidatos">
-            <section className='sect-info1'>
-                <Image className="foto-candidato" src={FotoTeste} alt="Foto de perfil do candidato"></Image>
+            <section className='cont1'>
+                    <section className='sect-info1'>
+                        <Image className="foto-candidato" src={FotoTeste} alt="Foto de perfil do candidato"></Image>
+                    </section>
+                    <section className='sect-info2'>
+                        <h2 className="titulos">Nome:</h2>
+                            <div className="sessoes">{dados.nome}</div>
+                        <h2 className="titulos">Contato:</h2>
+                            <div className="sessoes">{dados.contato}</div>
+                        <h2 className="titulos">CRE:</h2>
+                            <div className="sessoes">{dados.cre}</div>
+                    </section>
+                    <section className='sect-info3'>
+                        <h2 className="titulos">Email:</h2>
+                            <div className="sessoes">{dados.email}</div>
+                        <h2 className="titulos">Matrícula:</h2>
+                            <div className="sessoes">{dados.matricula}</div>
+                    </section>
             </section>
-            <section className='sect-info2'>
-                <h2 className="titulos">Nome:</h2>
-                    <div className="sessoes">{dados.nome}</div>
-                <h2 className="titulos">Contato:</h2>
-                    <div className="sessoes">{dados.contato}</div>
-                <h2 className="titulos">CRE:</h2>
-                    <div className="sessoes">{dados.cre}</div>
-            </section>
-            <section className='sect-info3'>
-                <h2 className="titulos">Email:</h2>
-                    <div className="sessoes">{dados.email}</div>
-                <h2 className="titulos">Matrícula:</h2>
-                    <div className="sessoes">{dados.matricula}</div>
+            <section className='cont2'>
+                <div>Teste</div>
             </section>
         </div>
     )
