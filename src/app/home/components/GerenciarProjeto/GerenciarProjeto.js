632299535091d9c1eaaa9901ae43f748031d28cf
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-03-03 11:55:54 -0300

    Correção de erro na funcionalidade de deletar projetos

--- src/app/home/components/GerenciarProjeto/GerenciarProjeto.js
+++ src/app/home/components/GerenciarProjeto/GerenciarProjeto.js
@@ -18,7 +18,22 @@ export default function GerenciarProjeto(){
 
     useEffect(() => {
         fetchProjetos();
-    }, [projetos]);
+    }, []); // Removendo o projeto do Array
+
+    //Código para resolver os problemas:
+
+    const handleDelete = async (id) => {
+        const response = await fetch(`/api/projetos/${id}`, {
+            method: 'DELETE'
+        });
+
+        if (response.ok) {
+            setProjetos(projetos.filter(p => p.id_projeto !== id)); // Atualiza o estado após excluir o projeto
+        } else {
+            // Tratar erro na exclusão
+            console.error('Erro ao excluir projeto:', response.status);
+        }
+    };
     
 
     return(
@@ -44,8 +59,7 @@ export default function GerenciarProjeto(){
                     instituicao={p.instituicao} 
                     descricao={p.descricao} 
                     situacao={p.situacao}
-                    onExclude={() => setProjetos([...projetos])}
-                    /*onExclude={() => setProjetos(projetos.filter(proj => proj.id_projeto !== p.id_projeto))}*/
+                    onExclude={() => handleDelete(p.id_projeto)}
                     >
               </Projeto>))}
 
