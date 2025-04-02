139cc8cab3a034f0f68867f8abf48f0c600ef155
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-04-01 22:55:46 -0300

    Correção de elementos da tela de apresentação

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,23 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import { Questrial, Radio_Canada } from "next/font/google";
 import NavBar from "../componentes/NavBar/NavBar";
-import Descricao from "../componentes/Descricao/Descricao";
-import Pergunta from "../componentes/Pergunta/Pergunta";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <NavBar></NavBar>
       <h1 className={styles.titulo}>RecruitSync</h1>
-        <Descricao></Descricao>
-        <Pergunta></Pergunta>
+
+      <div className={styles.descricao_rs}>
+        <p>"Explore oportunidades educacionais e inscreva-se para se tornar um bolsista! </p>
+        <p>Descubra oportunidades de desenvolvimento acadêmico e profissional sob medida</p>
+        <p>para você. Seja parte da próxima geração de líderes. Inscreva-se agora!"</p>
+      </div>
+
+      <div className={styles.perguntas}>
+          <h3 className={styles.pergunta}> <strong className={styles.destaque}>O que há no RecruitSync?</strong> Tudo que você precisa para ser um bolsista!</h3>
+      </div>
+      
     </main>
   );
 }
--- src/componentes/Pergunta/Pergunta.js
+++
@@ -1,9 +0,0 @@
-import './pergunta.css'
-
-export default function Pergunta(){
-    return(
-        <div className="pergunta">
-            <h3 className='perg'> <strong>O que há no RecruitSync?</strong> Tudo que você precisa para ser um bolsista!</h3>
-        </div>
-    )
-}
